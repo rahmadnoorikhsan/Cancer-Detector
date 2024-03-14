5cb9095d1995435c2f8ef352755c9c5be949e31f
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2024-03-14 15:42:36 +0700

    Initial Project

---
+++ app/src/main/java/com/rahmadev/detector/cancer/data/api/ApiService.kt
@@ -0,0 +1,15 @@
+package com.rahmadev.detector.cancer.data.api
+
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ApiService {
+
+    @Multipart
+    @POST("skin-cancer/predict")
+    suspend fun uploadImage(
+        @Part file: MultipartBody.Part
+    ): FileUploadResponse
+}
